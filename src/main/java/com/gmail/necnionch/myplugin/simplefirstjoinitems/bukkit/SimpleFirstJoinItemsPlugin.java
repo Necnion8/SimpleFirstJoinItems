47b6a4bcd1b4aeb4e88eff3ac14284f14a344016
Necnion8 <necnionch@gmail.com>
2023-05-01 18:21:53 +0900

    Initial commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/simplefirstjoinitems/bukkit/SimpleFirstJoinItemsPlugin.java
@@ -0,0 +1,17 @@
+package com.gmail.necnionch.myplugin.simplefirstjoinitems.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleFirstJoinItemsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
